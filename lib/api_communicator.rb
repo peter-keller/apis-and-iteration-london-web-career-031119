c7afad5c52a157fee877f80477459b56cc015ea1
github username <github email address>
2019-03-12 12:49:45 +0000

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,20 +6,33 @@ def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  movie_names = []
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  response_hash["results"].each do |person|
+    if person["name"].downcase.split(" ").include?(character_name)
+      movies = person["films"]
+      for movie in movies 
+        movie_names.push(getMovieTitle(movie))
+      end
+    end
+  end
+  movie_names
+end
+
+def getMovieTitle(url)
+  response_string = RestClient.get(url)
+  response_hash = JSON.parse(response_string)
+  response_hash["title"]
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+  if films.empty?
+    puts "No movie"
+  else
+    films.each_with_index do |item, index|
+      puts "#{index += 1}. #{item}"
+    end
+  end 
 end
 
 def show_character_movies(character)
